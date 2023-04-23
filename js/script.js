844df5beb3f3a1df6f8f9695e42b98dbd6a47c6c
Ivana Zandona <maribeliva2.0@gmail.com>
2023-04-23 01:20:54 -0300

    pag productos y medios de pago

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let ingreso2 = document.getElementById("2cuotas");
+let ingreso3 = document.getElementById("3cuotas");
+let boton2 = document.getElementById("boton2");
+let boton3 = document.getElementById("boton3");
+let total2 = document.getElementById("2cuotasFinal");
+let total3 = document.getElementById("3cuotasFinal");
+let entrega2 = document.getElementById("2cuotasEntrega");
+let entrega3 = document.getElementById("3cuotasEntrega");
+let cuotas2 = document.getElementById("2cuotasFinalFinal");
+let cuotas3 = document.getElementById("3cuotasFinalFinal");
+
+boton2.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("holaaaaaaaa");
+})
+
+boton3.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("holaaaaaaaa");
+})
+
+function calcular2() {
+    console.log("holaaaaa");
+}
+
+function calcular3() {
+    console.log("holaaaaaaaa");
+}
